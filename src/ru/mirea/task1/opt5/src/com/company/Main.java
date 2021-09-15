4eaba88e34a001981202d71cacbb3545df5a00b7
Sergey Pivovarov <leva-07@inbox.ru>
2021-09-15 17:45:53 +0300

    Задание 5

--- src/ru/mirea/task1/opt5/src/com/company/Main.java
+++ src/ru/mirea/task1/opt5/src/com/company/Main.java
@@ -1,69 +1,16 @@
 package com.company;
 import java.lang.*;
-import java.util.Scanner;
 
 public class Main {
 
-    public static void sumArr() {
-        System.out.println("Заполните массив чисел.");
-
-        int[] arr = new int [10];
-        int temp, sumFor = 0, sumWhile = 0, sumDoWhile = 0;
-        Scanner in = new Scanner(System.in);
-
-        for (int i = 0; i < arr.length; i++) {
-            System.out.print("Введите элемент с индесом " + "[" + i + "]: ");
-            arr[i] = in.nextInt();
+    public static void harmonic() {
+        System.out.println("Первые 10 чисел гармонического ряда");
+        for (int i = 0; i<10; i++) {
+            System.out.println(1/(double)(i + 1) + " ");
         }
-
-        for (int i = 0; i < 10; i++) {
-            sumFor += arr[i];
-        }
-
-        System.out.println("\nСумма всех элементов через for: " + sumFor);
-
-        int i = 0;
-
-        while (i < 10) {
-            sumWhile += arr[i++];
-        }
-
-        System.out.println("Сумма всех элементов через while: " + sumWhile);
-
-        i = 0;
-
-        do {
-            sumDoWhile += arr[i];
-        } while (++i < 10);
-
-        System.out.println("Сумма всех элементов через do while: " + sumFor + "\n");
-
-    }
-
-    public static int factorial(int n) {
-        int s = 1;
-
-        while (n > 1) {
-            s *= n--;
-        }
-
-        return s;
     }
 
 	public static void main(String[] args) {
-        sumArr();
-
-        System.out.print("Введите число для подсчета факториала: ");
-        Scanner in = new Scanner(System.in);
-
-        int n = 0;
-        n = in.nextInt();
-
-        while (n < 1) {
-            System.out.print("\nЧисло для факторила должно быть > 1, попробуйте ввести другое: ");
-            n = in.nextInt();
-        }
-
-        System.out.println("\nФакториал числа " + n + " равен: " + factorial(n));
+       harmonic();
     }
 }
