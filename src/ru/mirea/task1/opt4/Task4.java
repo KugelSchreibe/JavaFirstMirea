1d1a6bb4a64c9e52e8be18355967f64d1b7ccef3
Sergey Pivovarov <leva-07@inbox.ru>
2021-09-21 15:45:47 +0300

    lab1

--- src/ru/mirea/task1/opt4/Task4.java
+++ src/ru/mirea/task1/opt4/Task4.java
@@ -1,9 +1,8 @@
-package com.company;
+package ru.mirea.task1.opt4;
 import java.lang.*;
 
-public class Main {
-
-	public static void main(String[] args) {
+public class Task4 {
+    public static void main(String[] args) {
         System.out.println("Аргументы командной строки: ");
         for (int i = 0; i < args.length; i++) {
             System.out.print(args[i]);
--- src/ru/mirea/test/Main.java
+++ src/ru/mirea/test/Main.java
@@ -3,9 +3,10 @@ package ru.mirea.test;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
         System.out.println("Hi");
         System.out.println("Tester");
         System.out.println("Third");
+
+
     }
 }
